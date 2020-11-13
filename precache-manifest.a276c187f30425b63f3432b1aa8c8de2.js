d11cd80a9946131b8d03164579422ef898c22057
Elena_Bokhan <clevergirl_777@mail.ru>
2020-11-13 23:25:59 +0400

    Updates

---
+++ precache-manifest.a276c187f30425b63f3432b1aa8c8de2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50d4a74aa09ffb955f37ddd3d6698c0c",
+    "url": "/Minesweeper/index.html"
+  },
+  {
+    "revision": "aab0b720909dbeefe15e",
+    "url": "/Minesweeper/static/css/main.294e7169.chunk.css"
+  },
+  {
+    "revision": "34795dded6f47cb997ea",
+    "url": "/Minesweeper/static/js/2.4bdf232a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Minesweeper/static/js/2.4bdf232a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aab0b720909dbeefe15e",
+    "url": "/Minesweeper/static/js/main.fab7109c.chunk.js"
+  },
+  {
+    "revision": "bfee3e2e1b815320cc84",
+    "url": "/Minesweeper/static/js/runtime-main.e9fd0c09.js"
+  },
+  {
+    "revision": "c300850a6dbb2533ad79bb28daebda36",
+    "url": "/Minesweeper/static/media/Stravinski-Deco.c300850a.ttf"
+  },
+  {
+    "revision": "6ec2bab0c69bb37b2cc0eaec8f23461a",
+    "url": "/Minesweeper/static/media/background.6ec2bab0.jpg"
+  }
+]);
